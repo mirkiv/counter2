5f896f78a1e4aad0604006c989935174bb3592ac
mirkiv <mirkamolmizraahmedov@gmail.com>
2024-10-16 21:51:43 +0500

    Text

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let button = document.querySelector("#counter");
 let counter = 0;
+let text = document.querySelector("#text");
 button.addEventListener("click", btnFn);
 function btnFn() {
 	counter++;
 	console.log(counter);
+	text.textContent = counter;
 }
